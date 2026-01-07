2ae23f5ac703e289a6127716ca773ce1f05c46c1
Michael Hernandez <mikegtr_305@hotmail.com>
2026-01-07 16:29:22 -0500

    Initial Commit
    Attribute set added and created folders in UE for organization

---
+++ Source/GK/Private/GameplayAbilitySystem/AttributeSets/GKBasicAttributeSet.cpp
@@ -0,0 +1,56 @@
+//  Rodney Torres, Erik Aguiar, and Michael Hernandez All Rights
+
+
+#include "GameplayAbilitySystem/AttributeSets/GKBasicAttributeSet.h"
+#include "GameplayEffectExtension.h"
+
+UGKBasicAttributeSet::UGKBasicAttributeSet()
+{
+	Health = 100.f;
+	MaxHealth = 100.f;
+	Damage = 0.f;
+}
+
+// --- Clamp health to its maximum value before changes ---
+void UGKBasicAttributeSet::PreAttributeBaseChange(const FGameplayAttribute& Attribute, float& NewValue) const
+{
+	Super::PreAttributeBaseChange(Attribute, NewValue);
+
+	// Clamp health to its maximum value
+	if (Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxHealth());
+	}
+}
+
+// --- Handle damage application and health updates ---
+void UGKBasicAttributeSet::PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+
+	// Apply damage
+	if (Data.EvaluatedData.Attribute == GetDamageAttribute())
+	{
+		float TotalDamage = GetDamage();
+		SetDamage(0.f);
+
+		// Subtract damage from health
+		SetHealth(GetHealth() - TotalDamage);
+
+		// TODO: Activate hit reaction ability if health was modified
+	}
+
+	// Update health after gameplay effect execution
+	if (Data.EvaluatedData.Attribute == GetHealthAttribute())
+	{
+		SetHealth(GetHealth());
+	}
+}
+
+// --- Additional logic after attribute base changes ---
+void UGKBasicAttributeSet::PostAttributeBaseChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue) const
+{
+	Super::PostAttributeBaseChange(Attribute, OldValue, NewValue);
+
+	// Logic for attribute changes like handling Death Ability
+}
---
+++ Source/GK/Public/GameplayAbilitySystem/AttributeSets/GKBasicAttributeSet.h
@@ -0,0 +1,39 @@
+//  Rodney Torres, Erik Aguiar, and Michael Hernandez All Rights
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "GKBasicAttributeSet.generated.h"
+
+UCLASS()
+class GK_API UGKBasicAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+
+public:
+	UGKBasicAttributeSet();
+
+	// Health Attributes
+	UPROPERTY(BlueprintReadOnly, Category="Attributes")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS_BASIC(UGKBasicAttributeSet, Health)
+
+	UPROPERTY(BlueprintReadOnly, Category="Attributes")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS_BASIC(UGKBasicAttributeSet, MaxHealth)
+
+	// Damage Attributes
+	UPROPERTY(BlueprintReadOnly, Category="Attributes")
+	FGameplayAttributeData Damage;
+	ATTRIBUTE_ACCESSORS_BASIC(UGKBasicAttributeSet, Damage)
+
+public:
+	// Attribute Change Hooks
+	virtual void PreAttributeBaseChange(const FGameplayAttribute& Attribute, float& NewValue) const override;
+
+	virtual void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data) override;
+	
+	virtual void PostAttributeBaseChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue) const override;
+};
