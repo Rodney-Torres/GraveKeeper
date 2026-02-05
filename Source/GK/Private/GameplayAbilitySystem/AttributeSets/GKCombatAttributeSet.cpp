aa0dd3864fd069276a6c7cce70e0c7477911b5c7
Altodor <rodneytorres.dev@gmail.com>
2026-02-05 14:49:57 -0500

    Weapon added to enemy base

---
+++ Source/GK/Private/GameplayAbilitySystem/AttributeSets/GKCombatAttributeSet.cpp
@@ -0,0 +1,42 @@
+﻿//  Rodney Torres, Erik Aguiar, and Michael Hernandez All Rights
+
+#include "GameplayAbilitySystem/AttributeSets/GKCombatAttributeSet.h"
+#include "GameplayEffectExtension.h"
+
+UGKCombatAttributeSet::UGKCombatAttributeSet()
+{
+	Strength = 0.f;
+	MaxStrength = 100.f;
+	Defense = 0.f;
+	MaxDefense = 100.f;
+}
+
+void UGKCombatAttributeSet::PreAttributeBaseChange(const FGameplayAttribute& Attribute, float& NewValue) const
+{
+	Super::PreAttributeBaseChange(Attribute, NewValue);
+
+	// Clamp Defense and Strength to their maximum values
+	if (Attribute == GetDefenseAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxDefense());
+	}
+	else if (Attribute == GetStrengthAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxStrength());
+	}
+}
+
+void UGKCombatAttributeSet::PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+
+	// Update Defense and Strength after gameplay effect execution
+	if (Data.EvaluatedData.Attribute == GetDefenseAttribute())
+	{
+		SetDefense(GetDefense());
+	}
+	else if (Data.EvaluatedData.Attribute == GetStrengthAttribute())
+	{
+		SetStrength(GetStrength());
+	}
+}
---
+++ Source/GK/Private/GameplayAbilitySystem/GKGameplayAbility.cpp
@@ -0,0 +1,10 @@
+﻿//  Rodney Torres, Erik Aguiar, and Michael Hernandez All Rights
+
+#include "GKGameplayAbility.h"
+
+UGKGameplayAbility::UGKGameplayAbility()
+{
+    // Define default ability tags
+    ActivationOwnedTags.AddTag(FGameplayTag::RequestGameplayTag(FName("GameplayAbility.Active")));
+    ActivationBlockedTags.AddTag(FGameplayTag::RequestGameplayTag(FName("State.Dead")));
+}
